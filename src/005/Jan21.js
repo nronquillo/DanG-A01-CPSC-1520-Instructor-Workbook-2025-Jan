135917e5dce02a672203f977dbaebfb0ec71f515
Dan Gilleland <dgilleland@nait.ca>
2025-01-21 10:37:51 -0700

    Trying stuff in a node repl

---
+++ src/005/Jan21.js
@@ -0,0 +1,42 @@
+console.clear()
+const pi = 3.14159
+pi
+let firstNumber = 8
+let secondNumber
+firstNumber
+secondNumber
+secondNumber = 5
+firstNumber + secondNumber
+firstNumber - secondNumber
+firstNumber * secondNumber
+firstNumber / secondNumber
+firstNumber % secondNumber
+7 % secondNumber
+console.clear()
+typeof pi
+typeof firstNumber
+let name = 'Dan Gilleland'
+typeof name
+name
+pi
+firstNumber = 'eight'
+typeof firstNumber
+firstNumber + secondNumber
+firstNumber = '8'
+firstNumber + secondNumber
+firstNumber * secondNumber
+console.clear()
+12 * 'bob'
+12 * '6bob'
+parseInt('5.95')
+parseFloat('5.95')
+parseInt('6bob')
+Math.PI
+console.clear()
+console.clear()
+0.1 + 0.1 + 0.1
+0.1 + 0.1 + 0.1 + 0.1
+let answer = 0.1 + 0.1 + 0.1
+answer
+answer.toString()
+answer.toFixed(2)
