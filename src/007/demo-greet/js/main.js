72943cf38c8fed434fb142fd5ac4af26532b6b97
Dan Gilleland <dgilleland@nait.ca>
2025-01-21 11:35:53 -0700

    Preferred way to create functions

--- src/007/demo-greet/js/main.js
+++ src/007/demo-greet/js/main.js
@@ -2,9 +2,17 @@
 // e.g. greetUser('Jane Doe');
 // e.g. greetUser(prompt('Name:'));
 
-function greetUser(username) {
+/**
+ * greetUser() will personalize the h1.intro element with a Welcome messge.
+ * @param {string} username The name of someone
+ */
+const greetUser = function (username) { // the function is an anonymous function
     // select the h1
     var mainTitle = document.querySelector('h1.intro');
     // update the h1 inner HTML
     mainTitle.innerHTML = 'Welcome ' + username + ' to ' + mainTitle.innerHTML;
 };
+
+
+// Now I will call my greetUser function
+greetUser('Uncle Bob');
