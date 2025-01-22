ba744e5e0921efc7b33c57bfe4c2778aed291bf7
Dan Gilleland <dgilleland@nait.ca>
2025-01-22 12:34:55 -0700

    Starter kit updates

---
+++ sk/008/demo-text-manipulation/js/main.js
@@ -0,0 +1,43 @@
+// Some global variables that we will re-use frequently
+const userInput = document.getElementById('userInput');
+const output = document.getElementById('transformedText');
+// Show the user input in the output
+output.innerText = userInput.value;
+// A little prep....
+const presetPaddingLength = function() {
+    let input = document.getElementById('minLength');
+    input.value = userInput.value.length + 5;
+}
+presetPaddingLength();
+
+// And now for the lesson....
+document.getElementById('transformToUpperCase').addEventListener('click', function() {
+    let newValue = userInput.value.toUpperCase();
+    //             \  string     /
+    output.innerText = newValue;
+});
+
+// TODO: 1) Write the code to transform the user's input to lower case and display it
+//       STUDENT_CODE_HERE
+
+document.querySelector('#transformPadEnd').addEventListener('click', function() {
+    // TODO: 2) Modify the code below to use the appropriate user input for padding.
+    let currentValue = 'bob';// userInput.value;
+    let newValue = currentValue.padEnd(10, '^');
+    output.innerText = newValue;
+});
+
+// TODO: 3) Write the code to output the input text with spaces trimmed from the left-hand side.
+
+// TODO: 4) Write the code to output the input text with spaces trimmed from the right-hand side.
+
+// TODO: 5) Write the code to output the input text with spaces trimmed from the both sides.
+
+// TODO: 6) Write the code to pad the start of input text with the supplied character(s).
+
+// TODO: 7) Write the code to pad the end of input text with the supplied character(s).
+
+// TODO: 8) Write the code to replace text in the user's input.
+
+// TODO: 6) Write the code to repeat the text the specified number of times on separate lines.
+
