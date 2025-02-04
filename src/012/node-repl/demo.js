a139dc8479ea5303f48dc945d7ec5500fda519d5
Dan Gilleland <dgilleland@nait.ca>
2025-02-04 10:43:08 -0700

    Demo conditional expressions in Node REPL

---
+++ src/012/node-repl/demo.js
@@ -0,0 +1,26 @@
+let textZero = '0';
+let numberZero = 0;
+let numberEight = 8;
+let numberFive = 5;
+let textEight = '8';
+let name = 'Stewart';
+let textDate = '2025-02-28'; // ISO format, date only
+// let textTime = '07:43:13.340Z'; // ISO format, time only
+// let textDateTime = "2025-02-28T07:43:07.620Z";
+let success = true;
+let failure = false;
+
+// numbers as strings
+let oneThousand = '1000';
+let seven = '7';
+
+(numberEight == textEight)
+(numberEight === textEight)
+(textEight < numberFive)
+(textEight > numberFive)
+if(name) console.log('That is true')
+if('') { console.log('Is that true?'); } else { console.log('That is false!'); }
+(seven > oneThousand)
+if(numberEight = 8) console.log('That is true.')
+if(numberEight = 5) console.log('That is also true?')
+numberEight
