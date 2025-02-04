7a21e8fb1ba5af5cc50a218138c82db24dba96bb
Dan Gilleland <dagilleland@users.noreply.github.com>
2025-02-04 01:59:37 -0700

    Create data.js

---
+++ sk/012/NodeRepl/data.js
@@ -0,0 +1,15 @@
+let textZero = '0';
+let numberZero = 0;
+let numberEight = 8;
+let numberFive = 5;
+let textEight = '8';
+let name = 'Stewart';
+let textDate = '2025-02-28'; // ISO format, date only
+// let textTime = '07:43:13.340Z'; // ISO format, time only
+// let textDateTime = "2025-02-28T07:43:07.620Z";
+let success = true;
+let failure = false;
+
+// numbers as strings
+let oneThousand = '1000';
+let seven = '7';
